86fc3d73c1a0060d89193f5c9bcf2e089850f93e
Vilux00 <valerio.ghirimoldi@gmail.com>
2021-12-09 21:28:16 +0100

    Added Password class

--- EVoteJavafx/src/main/java/main/Password.java
+++ EVoteJavafx/src/main/java/main/Password.java
@@ -1,7 +1,17 @@
 package main;
 
+import java.security.SecureRandom;
+import org.apache.commons.lang3.RandomStringUtils;
+
 public class Password {
 	public static String generaPasswordRandom() {
-		return null;
+		char[] caratteriUsabili = (new String("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&*-_=+?")).toCharArray();
+		String pwdRandom = RandomStringUtils.random( 10, 0, caratteriUsabili.length-1, false, false, caratteriUsabili, new SecureRandom() );
+		return pwdRandom;
+	}
+	
+	public static boolean controlloPassword(String pwd) {
+		if(pwd.length() > 8 && pwd.replaceAll("A-Za-z0-9", "").length() > 0 && pwd.replaceAll("A-Za-z/[^\\p{L}\\d\\s@#]/u", "").length() > 0) return true;
+		return false;
 	}
 }
