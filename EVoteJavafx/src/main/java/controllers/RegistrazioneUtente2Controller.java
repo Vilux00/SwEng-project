ba50f9eb2ef08ac8b41881b1a4ec77194578a65e
loremiglie <lorenzo.miglietta@studenti.unimi.it>
2021-12-10 09:42:53 +0100

    set generaPassword code

--- EVoteJavafx/src/main/java/controllers/RegistrazioneUtente2Controller.java
+++ EVoteJavafx/src/main/java/controllers/RegistrazioneUtente2Controller.java
@@ -1,4 +1,23 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import main.Password;
+
 public class RegistrazioneUtente2Controller  extends DefaultSceneController{
+	
+	private String password;
+	
+	@FXML
+	private TextField visualizzaPassword;
+	
+	public void generaPassword(ActionEvent event) {
+		password = Password.generateRandomPassword();
+		visualizzaPassword.setText(password);
+	}
+	
+	public void registraUtenteDB() {
+		//chiamata al db
+	}
 }
