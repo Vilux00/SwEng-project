03de114192ec6c79f0d4267046a3262e1a7c59c4
Vilux00 <valerio.ghirimoldi@gmail.com>
2021-12-10 08:51:11 +0100

    Updated ModificaPasswordController class

--- EVoteJavafx/src/main/java/controllers/ModificaPasswordController.java
+++ EVoteJavafx/src/main/java/controllers/ModificaPasswordController.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-import java.util.Optional;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -10,7 +9,6 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
