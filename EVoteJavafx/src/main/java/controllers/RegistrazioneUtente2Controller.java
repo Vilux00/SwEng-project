f4a92dc830bb0f976aa93cc781b064cabe1988db
loremiglie <lorenzo.miglietta@studenti.unimi.it>
2021-12-10 10:03:57 +0100

    generazione password

--- EVoteJavafx/src/main/java/controllers/RegistrazioneUtente2Controller.java
+++ EVoteJavafx/src/main/java/controllers/RegistrazioneUtente2Controller.java
@@ -2,7 +2,9 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import main.Password;
 
 public class RegistrazioneUtente2Controller  extends DefaultSceneController{
@@ -12,12 +14,28 @@ public class RegistrazioneUtente2Controller  extends DefaultSceneController{
 	@FXML
 	private TextField visualizzaPassword;
 	
+	@FXML
+	private HBox boxCreaAccount;
+	
+	@FXML
+	private HBox stampaPdfAccount;
+	
+	@FXML
+	private Button btnGeneraPassword;
+	
 	public void generaPassword(ActionEvent event) {
 		password = Password.generateRandomPassword();
 		visualizzaPassword.setText(password);
+		boxCreaAccount.setVisible(true);
 	}
 	
-	public void registraUtenteDB() {
+	public void registraUtenteDB(ActionEvent event) {
+		btnGeneraPassword.setDisable(false);   //da sistemare
 		//chiamata al database
+		stampaPdfAccount.setVisible(true);
+	}
+	
+	public void stampaCredenzialiUtente(ActionEvent event) {
+		System.out.println("Stampa credenziali utente");
 	}
 }
