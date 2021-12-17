ae7ef92d51a9ff9ad50dfb9369d9dbe882ce4b5c
Vilux00 <valerio.ghirimoldi@gmail.com>
2021-12-17 17:17:07 +0100

    Updated DbManager

--- EVoteJavafx/src/main/java/data/DbManager.java
+++ EVoteJavafx/src/main/java/data/DbManager.java
@@ -1,11 +1,12 @@
 package data;
 
 import java.sql.*;
+import org.apache.commons.codec.digest.DigestUtils;
 
 public class DbManager {
     private static DbManager dbMgr;
-    private static String connString = "jdbc:postgresql://localhost/eVoting-platform?user=user_1&password=password&ssl=false"; //Not working with ssl=true
-
+    private String connString = "jdbc:postgresql://localhost/eVoting-platform?user=user_1&password=password&ssl=false"; //Not working with ssl=true
+    
     public static DbManager getInstance(){
         if(dbMgr == null) {
             dbMgr = new DbManager();
@@ -13,13 +14,21 @@ public class DbManager {
         return dbMgr;
     }
 
-    public Connection open(){
+    private Connection open(){
         try{
             return DriverManager.getConnection(connString);
         }catch(SQLException e){
             e.printStackTrace();
             return null;
         }
+    }
+    
+    private void close(Connection c) {
+    	try {
+			c.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
     }
 
     public void testDB(){
@@ -31,6 +40,22 @@ public class DbManager {
             e.printStackTrace();
         }
     }
+
+	public boolean login(String codF, String password) {
+		Connection conn = open();
+		try {
+	        PreparedStatement stm = conn.prepareStatement("SELECT * FROM evoting.elettore WHERE username = ? AND password = ?");
+	        stm.setString(1, codF);
+	        stm.setString(2, DigestUtils.sha256Hex(password));
+	        ResultSet r = stm.executeQuery();
+	        return r.next();
+		} catch(SQLException ex){
+			ex.printStackTrace();
+			return false;
+		} finally {
+			close(conn);
+		}
+	}
 
 
 }
