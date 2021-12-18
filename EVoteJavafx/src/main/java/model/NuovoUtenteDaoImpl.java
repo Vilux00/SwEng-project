b7ddbd987f622c003081159b5b88253b7f57f1dc
Vilux00 <valerio.ghirimoldi@gmail.com>
2021-12-18 14:43:34 +0100

    Updated NuovoUtenteDaoImpl

--- EVoteJavafx/src/main/java/model/NuovoUtenteDaoImpl.java
+++ EVoteJavafx/src/main/java/model/NuovoUtenteDaoImpl.java
@@ -1,5 +1,12 @@
 package model;
 
 public class NuovoUtenteDaoImpl implements NuovoUtenteDao{
-
+    private String nome;
+    private String cognome;
+    private String codF;
+    private String dataNascita;
+    private String paeseNascita;
+    private String nazioneNascita;
+    private char sesso;
+    private char privilegio;
 }
