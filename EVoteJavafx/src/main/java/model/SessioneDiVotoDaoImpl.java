43759ccda4c61cd03f99d4e67e954879fc48dead
Vilux00 <valerio.ghirimoldi@gmail.com>
2021-12-18 21:07:50 +0100

    Updated SessioneDiVotoDaoImpl class

--- EVoteJavafx/src/main/java/model/SessioneDiVotoDaoImpl.java
+++ EVoteJavafx/src/main/java/model/SessioneDiVotoDaoImpl.java
@@ -1,9 +1,22 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import data.DbManager;
+
 public class SessioneDiVotoDaoImpl implements SessioneDiVotoDao{
 
 	@Override
 	public void inserisciSessione(SessioneDiVoto s) {
-		//DbManager.getInstance().query("QUERY =>");
+		DbManager dbM = DbManager.getInstance();
+		Connection c = dbM.open();
+		try {
+			c.prepareStatement("INSERT INTO evoting.sessione_voto VALUES(?, ?, ?, ?, ?, ?, ?)");
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			dbM.close(c);
+		}
 	}
 }
