27493ce5394f2a2b1c1fafab6b51e0e25c701abc
Vilux00 <valerio.ghirimoldi@gmail.com>
2021-12-18 14:31:01 +0100

    Updated ModificaPasswordHandler

--- EVoteJavafx/src/main/java/controllers/ModificaPasswordHandler.java
+++ EVoteJavafx/src/main/java/controllers/ModificaPasswordHandler.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.Alert.AlertType;
+import model.DaoFactory;
 import model.Elettore;
 import model.ElettoreDaoImpl;
 import model.Password;
@@ -20,6 +21,51 @@ public class ModificaPasswordHandler extends DefaultSceneHandler{
 	PasswordField nuovaPassword2;
 	
 	public void modificaPassword(ActionEvent event) throws IOException {
+		String oldPw = vecchiaPassword.getText();
+		String newPw1 = nuovaPassword1.getText();
+		String newPw2 = nuovaPassword2.getText();
+		Elettore e = (Elettore)data;
+
+		if(oldPw.equals("") || newPw1.equals("") || newPw2.equals("")){
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Campi vuoti");
+			alert.setHeaderText("Riempire tutti i campi");
+			alert.show();
+		}
+		else if(!checkOldPassword()){
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Password incorretta");
+			alert.setHeaderText("La vecchia password è incorretta");
+			alert.show();
+		}
+		else if(!newPw1.equals(newPw2)){
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Password non conincidenti");
+			alert.setHeaderText("I campi nuova password non coincidono");
+			alert.show();
+		}
+		else if(!Password.checkPassword(newPw1)){
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Password non conforme");
+			alert.setHeaderText("La nuova password non rispetta i requisiti");
+			alert.show();
+		}
+		else if(newPw1.equals(e.getPassword())){
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setHeaderText("La nuova password e' identica alla precedente");
+			alert.setTitle("Password identiche");
+			alert.show();
+		}
+		else{
+			if(changePasswordDB()){
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setHeaderText("Password modificata con successo");
+				alert.setTitle("Password modificata");
+				alert.show();
+			}
+		}
+	}
+		/*
 		if(!vecchiaPassword.getText().isEmpty() && checkOldPassword() == true){
 			if(nuovaPassword1.getText().equals(nuovaPassword2.getText())) {
 				if(Password.checkPassword((nuovaPassword1.getText()))) {
@@ -37,24 +83,17 @@ public class ModificaPasswordHandler extends DefaultSceneHandler{
 			alert.setTitle("Vecchia password incorretta");
 			alert.show();
 		}
-	}
+		*/
 	
 	private boolean checkOldPassword() {
 		Elettore e = (Elettore)data;
-		return e.getPassword() == vecchiaPassword.getText();
+		return e.getPassword().equals(vecchiaPassword.getText());
 	}
 	
-	private void changeToNewPassword() {
+	private boolean changePasswordDB() {
 		Elettore e = (Elettore)data;
-		if(e.getPassword() == nuovaPassword1.getText()) {
-			Alert alert = new Alert(AlertType.ERROR);
-			alert.setHeaderText("La nuova password e' identica alla precedente");
-			alert.setTitle("Password identiche");
-			alert.show();
-		}else {
-			e.setPassword(nuovaPassword1.getText());
-			new ElettoreDaoImpl().updatePassword(e);
-		}
+		e.setPassword(nuovaPassword1.getText());
+		ElettoreDaoImpl el = (ElettoreDaoImpl)DaoFactory.getInstance().getDao("Elettore");
+		return(el.updatePassword(e));
 	}
-
 }
