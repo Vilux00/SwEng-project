ebddc5fb8804795d62bdcaadfd44ea279d1776c0
loremiglie <lorenzo.miglietta@studenti.unimi.it>
2021-12-18 18:56:28 +0100

    added scene and controller (registra elettore a sessione)

---
+++ EVoteJavafx/src/main/java/controllers/RegistraElettoreASessioneController.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class RegistraElettoreASessioneController extends DefaultSceneHandler{
+
+    @FXML
+    private ComboBox<String> ComboBoxSessioni;
+
+    @FXML
+    private TextField TextFieldCodFisc;
+
+    @FXML
+    void aggiungiElettoreASessione(ActionEvent event) {
+    	
+    }
+
+}
