8d26a5b749fa2469c59b44bb7dbb3411ac493413
Vilux00 <valerio.ghirimoldi@gmail.com>
2021-12-19 10:00:48 +0100

    Updated LoginElettoreHandler

--- EVoteJavafx/src/main/java/controllers/LoginElettoreHandler.java
+++ EVoteJavafx/src/main/java/controllers/LoginElettoreHandler.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
+import model.DaoFactory;
 import model.Elettore;
 import model.ElettoreDaoImpl;
 
@@ -27,7 +28,8 @@ public class LoginElettoreHandler extends DefaultSceneHandler{
 			return;
 		}
 		Elettore e = new Elettore(codFisc, pwd);
-		if(new ElettoreDaoImpl().login(e)){
+		ElettoreDaoImpl ed = (ElettoreDaoImpl) DaoFactory.getInstance().getDao("Elettore");
+		if(ed.login(e)){
 			setScenaPrecedente("loginElettoreView.fxml", "Login elettore"); 
 			changeScene(event, "profiloElettoreView.fxml", "Home profilo", e);
 			DefaultSceneHandler.isLogged = true;
