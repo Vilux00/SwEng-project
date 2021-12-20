7439f9d2ed6c3afe9d472fee08361794376addae
loremiglie <lorenzo.miglietta@studenti.unimi.it>
2021-12-20 20:07:11 +0100

    added scheda bianca

--- EVoteJavafx/src/main/java/controllers/VotazioneReferendumController.java
+++ EVoteJavafx/src/main/java/controllers/VotazioneReferendumController.java
@@ -11,8 +11,9 @@ import javafx.scene.control.Alert.AlertType;
 public class VotazioneReferendumController extends DefaultSceneController{
 	public void setFavorevole(ActionEvent event) throws IOException{
 		Alert alert = new Alert(AlertType.CONFIRMATION);
-		alert.setHeaderText("Conferma dati");
-		alert.setTitle("Conferma dati");
+		alert.setHeaderText("Conferma votazione");
+		alert.setTitle("Conferma votazione");
+		alert.setContentText("Sicuro di voler votare 'favorevole' al referendum?");
 		Optional<ButtonType> result = alert.showAndWait();
 		if (result.isPresent() && result.get() == ButtonType.OK) {
 			//aggiorna db
@@ -24,8 +25,23 @@ public class VotazioneReferendumController extends DefaultSceneController{
 	
 	public void setContrario(ActionEvent event) throws IOException{
 		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setHeaderText("Conferma votazione");
+		alert.setTitle("Conferma votazione");
+		alert.setContentText("Sicuro di voler votare 'contrario' al referendum?");
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.isPresent() && result.get() == ButtonType.OK) {
+			//aggiorna db
+			//output visivo
+			rimuoviScenaPrecedente();
+			goToScenaPrecedente(event);
+		}
+	}
+	
+	public void setSchedaBianca(ActionEvent event) throws IOException{
+		Alert alert = new Alert(AlertType.CONFIRMATION);
 		alert.setHeaderText("Conferma dati");
 		alert.setTitle("Conferma dati");
+		alert.setContentText("Sicuro di voler lasciare la scheda bianca?");
 		Optional<ButtonType> result = alert.showAndWait();
 		if (result.isPresent() && result.get() == ButtonType.OK) {
 			//aggiorna db
