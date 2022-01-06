21d41237c1a13dcd7bd15c2873f76716936bbb9a
Vilux00 <valerio.ghirimoldi@gmail.com>
2022-01-06 10:27:24 +0100

    Created test classes

---
+++ EVoteJavafx/src/test/java/SessioneDiVotoTest.java
@@ -0,0 +1,7 @@
+import org.junit.*;
+
+public class SessioneDiVotoTest {
+	//Test modalita referendum => candidati e partiti null
+	//Test modalita altra => quesito null e candidati != null o partiti != null
+	//Test scrutinio avviato => scadenza passata
+}
